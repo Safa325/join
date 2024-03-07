c7b9f9d6dcf6d2cb1649623f90cbf816030869b3
RichardPeda <richard.peda@gmx.net>
2024-03-07 23:01:34 +0100

    addTask category dropdown function

--- JS/addTask.js
+++ JS/addTask.js
@@ -1,4 +1,4 @@
-profileBadgeColors =[
+profileBadgeColors = [
     '#FF7A00',
     '#FF5EB3',
     '#6E52FF',
@@ -18,9 +18,49 @@ profileBadgeColors =[
 
 
 
-function toggleDropdown(event) {
-    console.log(event.target)
-    let element = event.target;
+function toggleDropdown(id, dropdown) {
+    let element = document.getElementById(id)
     element.classList.toggle('arrow-up')
+
+    let dropContainer = document.getElementById(dropdown);
+    dropContainer.classList.toggle('d-none')
+
+}
+
+
+function selectPrio(prio) {
+    let containerUrgent = document.getElementById('addTask-prio-urgent');
+    let containerMedium = document.getElementById('addTask-prio-medium');
+    let containerLow = document.getElementById('addTask-prio-low');
+
+    containerUrgent.classList.remove('selected')
+    containerMedium.classList.remove('selected')
+    containerLow.classList.remove('selected')
+
+    if (prio == 'urgent') {
+        containerUrgent.classList.add('selected')
+    } else if (prio == 'medium') {
+        containerMedium.classList.add('selected')
+    }
+    else {
+        containerLow.classList.add('selected')
+    }
+}
+
+function selectCategory(event) {
+    let inputField = document.getElementById('addTask-category')
+    let text = '';
+    let element = event.target.querySelector("p")
+    if (element == null) {
+        text = event.target.innerHTML;
+
+    } else {
+        text = element.innerHTML;
+    }
+
+    inputField.value = text;
+
+    toggleDropdown('arrow-category', 'category-dropdown-container')
+
 
 }
