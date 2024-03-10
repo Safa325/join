c25f975a9c427b039b46e94a15076b5980bec52f
RichardPeda <richard.peda@gmx.net>
2024-03-10 01:42:10 +0100

    addTask subtask details

--- JS/addTask.js
+++ JS/addTask.js
@@ -142,7 +142,7 @@ function toggleContactSelection(index) {
     renderBadges()
 }
 
-function createTask(event) {
+function createTask() {
     let titleField = document.getElementById('addTask-input-title');
     let descriptionField = document.getElementById('addTask-input-description');
     let dateField = document.getElementById('addTask-input-date');
@@ -168,6 +168,7 @@ function createTask(event) {
 function createSubtask() {
     let subtask = document.getElementById('addTask-subtask-input');
     console.log('fire')
+    console.log(subtask.value)
     if (subtask.value) {
         subtasks.push(subtask.value);
     }
@@ -196,18 +197,89 @@ function disableSubtaskInput() {
     document.getElementById('addTask-delete-accept-container').classList.add('d-none')
 }
 
+function disableSubtaskInputDelayed() {
+    setTimeout(() => {
+        disableSubtaskInput()
+    }, 100);
+}
+
 
 function enableEditSubtask(index) {
+    removePseudo(index)
     let input = document.getElementById(`addTask-subtask-listElement-input_${index}`);
-    input.readOnly = true;
+    input.readOnly = false;
     input.classList.add('inputField-enabled')
     input.focus()
+
+    document.getElementById(`subtask-edit_${index}`).classList.add('d-none')
+    document.getElementById(`subtask-accept_${index}`).classList.remove('d-none')
 }
 
-function editSubtask(index) {
+function disableEditSubtask(index) {
+    let input = document.getElementById(`addTask-subtask-listElement-input_${index}`);
+    // input.readOnly = true;
+    addPseudo(index)
+    input.classList.remove('inputField-enabled')
+    document.getElementById(`subtask-edit_${index}`).classList.remove('d-none')
+    document.getElementById(`subtask-accept_${index}`).classList.add('d-none')
+}
+
+
+function editSubtask(event, index) {
     let input = document.getElementById(`addTask-subtask-listElement-input_${index}`);
     subtasks.splice(index, 1, input.value)
+    event.preventDefault();
+    // console.log(event)
+    // console.log(event.keyCode)
+
+    // if (event.keyCode === 13) {
+    //     disableEditSubtask(index)
+    // }
+
 }
+
+window.addEventListener('keyup',(event) => {   
+    console.log(event.key) 
+
+    if(event.key == 'Enter'){
+        indirectDisableEditSubtask();
+        // indirectCreateSubtask(); 
+        console.log('Enterkey') 
+    }
+      
+});
+
+// window.addEventListener("keyup", indirectDisableEditSubtask)
+// window.addEventListener("click", indirectDisableEditSubtask)
+
+function indirectDisableEditSubtask() {
+    let activeElement = this.document.activeElement;
+    // console.log(activeElement)
+    let elements = document.querySelectorAll('.addTask-subtask-listElement')
+    elements.forEach((element, index) => {
+        // if (activeElement != element) {
+            disableEditSubtask(index)
+        // }
+    });
+}
+
+function createTaskEnter(event) {
+    if(event.key === 'Enter') {
+        createSubtask()    
+    }
+}
+
+function indirectCreateSubtask(){
+    let activeElement = this.document.activeElement;
+    let subtask = document.getElementById('addTask-subtask-input')
+    if (activeElement == subtask) {
+        // disableSubtaskInputDelayed()
+        console.log('indirectCreateSubtask')
+        createSubtask()
+    }
+}
+
+
 
 function deleteSubtask(index) {
     subtasks.splice(index, 1);
@@ -224,24 +296,30 @@ function renderSubtasks() {
     }
 }
 
-function removePseudo(event) {
-    let element = event.target
-    element.parentElement.classList.remove('dot-before')
+function removePseudo(index) {
+    let subtaskElement = document.getElementById(`addTask-subtask-element_${index}`);
+    subtaskElement.classList.remove('dot-before')
+    subtaskElement.classList.add('subtask-element-selected')
+
 }
 
-function addPseudo(event, index) {
-    let element = event.target
-    element.parentElement.classList.add('dot-before')
+function addPseudo(index) {
+    let subtaskElement = document.getElementById(`addTask-subtask-element_${index}`);
+    subtaskElement.classList.add('dot-before')
+    subtaskElement.classList.remove('subtask-element-selected')
+
     let input = document.getElementById(`addTask-subtask-listElement-input_${index}`);
-    input.disabled = true;
+    input.readOnly = true;
 
 }
+
+
 
 function subTaskHTML(subtask, index) {
     return /*html*/`
-        <div class="addTask-subtask-element dot-before">
-        <input readonly="true" onkeyup="editSubtask(${index})" onblur="addPseudo(event, ${index})" onfocus="removePseudo(event)" id="addTask-subtask-listElement-input_${index}" class="addTask-subtask-listElement" type="text" value="${subtask}">
-            <div onclick="enableEditSubtask(${index})" class="subtask-icon subtask-edit">
+        <div class="addTask-subtask-element dot-before" id="addTask-subtask-element_${index}">
+        <input readonly="true" onkeyup="editSubtask(event, ${index})"  ondblclick="enableEditSubtask(${index})" id="addTask-subtask-listElement-input_${index}" class="addTask-subtask-listElement" type="text" value="${subtask}">
+            <div onclick="enableEditSubtask(${index})" class="subtask-icon subtask-edit" id="subtask-edit_${index}">
                 <svg width="25" height="25" viewBox="0 0 25 25" fill="none"
                     xmlns="http://www.w3.org/2000/svg">
                     <path
@@ -255,6 +333,12 @@ function subTaskHTML(subtask, index) {
                     <path
                         d="M3 18C2.45 18 1.97917 17.8042 1.5875 17.4125C1.19583 17.0208 1 16.55 1 16V3C0.716667 3 0.479167 2.90417 0.2875 2.7125C0.0958333 2.52083 0 2.28333 0 2C0 1.71667 0.0958333 1.47917 0.2875 1.2875C0.479167 1.09583 0.716667 1 1 1H5C5 0.716667 5.09583 0.479167 5.2875 0.2875C5.47917 0.0958333 5.71667 0 6 0H10C10.2833 0 10.5208 0.0958333 10.7125 0.2875C10.9042 0.479167 11 0.716667 11 1H15C15.2833 1 15.5208 1.09583 15.7125 1.2875C15.9042 1.47917 16 1.71667 16 2C16 2.28333 15.9042 2.52083 15.7125 2.7125C15.5208 2.90417 15.2833 3 15 3V16C15 16.55 14.8042 17.0208 14.4125 17.4125C14.0208 17.8042 13.55 18 13 18H3ZM3 3V16H13V3H3ZM5 13C5 13.2833 5.09583 13.5208 5.2875 13.7125C5.47917 13.9042 5.71667 14 6 14C6.28333 14 6.52083 13.9042 6.7125 13.7125C6.90417 13.5208 7 13.2833 7 13V6C7 5.71667 6.90417 5.47917 6.7125 5.2875C6.52083 5.09583 6.28333 5 6 5C5.71667 5 5.47917 5.09583 5.2875 5.2875C5.09583 5.47917 5 5.71667 5 6V13ZM9 13C9 13.2833 9.09583 13.5208 9.2875 13.7125C9.47917 13.9042 9.71667 14 10 14C10.2833 14 10.5208 13.9042 10.7125 13.7125C10.9042 13.5208 11 13.2833 11 13V6C11 5.71667 10.9042 5.47917 10.7125 5.2875C10.5208 5.09583 10.2833 5 10 5C9.71667 5 9.47917 5.09583 9.2875 5.2875C9.09583 5.47917 9 5.71667 9 6V13Z"
                         fill="#2A3647" />
+                </svg>
+            </div>
+            <div onclick="disableEditSubtask(${index})" class="subtask-icon subtask-accept d-none" id="subtask-accept_${index}">
+                <svg width="38" height="30" viewBox="0 0 38 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.02832 15.0001L15.2571 26.0662L33.9717 3.93408" stroke="#2A3647" stroke-width="7"
+                    stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
             </div>
         </div>
