f1295530e32ad2e3a722f90619c2a049cc0c356c
Safa325 <shamarisafa@gmail.com>
2024-03-14 17:07:53 +0100

    add hover effect

--- JS/summary.js
+++ JS/summary.js
@@ -21,3 +21,27 @@ function greetingHtml2(name) {
   return /*html*/ `<p>Good morning,</p>
   <h2>${name}</h2>`;
 }
+
+const toDoCard = document.getElementById("toDoCard");
+
+toDoCard.addEventListener("mouseover", function () {
+  const pencil = document.getElementById("pencil");
+  pencil.src = "./img/icons/pencil-white-r.svg";
+});
+
+toDoCard.addEventListener("mouseout", function () {
+  const pencil = document.getElementById("pencil");
+  pencil.src = "./img/icons/pencil-dark.svg";
+});
+
+const doneCard = document.getElementById("doneCard");
+
+doneCard.addEventListener("mouseover", function () {
+  const done = document.getElementById("done");
+  done.src = "./img/icons/done-white.svg";
+});
+
+doneCard.addEventListener("mouseout", function () {
+  const done = document.getElementById("done");
+  done.src = "./img/icons/done-dark.svg";
+});
