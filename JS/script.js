5052646472d4ffcf218e18234dd6e6ef1f6ce3d6
Safa325 <shamarisafa@gmail.com>
2024-03-23 23:04:52 +0100

    s

--- JS/script.js
+++ JS/script.js
@@ -138,7 +138,6 @@ function removeMenu() {
 function firstLoad() {
   if (!sessionStorage.getItem("firstLoad")) {
     window.location.href = "login.html";
-    sessionStorage.setItem("firstLoad", true);
   }
 }
 
